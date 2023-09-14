f5a63f06d796879b9e593fe862a3e86b36b20546
YourNewUsername <YourNewEmailAddress@example.com>
2023-09-14 17:52:32 -0300

    cors

---
+++ src/main/java/Rockwell/CRUD/CorsConfig.java
@@ -0,0 +1,28 @@
+package Rockwell.CRUD;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from any origin
+        config.addAllowedOrigin("*");
+
+        // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+        config.addAllowedMethod("*");
+
+        // Allow all headers
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
